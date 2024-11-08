c109d4565ab07d23b7314a5739c9a2092cdc5e62
Pavlo Batov <batovpasha@gmail.com>
2024-11-08 09:52:59 +0200

    feat: initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to pre-commit-jira
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/pre_commit_jira/core.clj
@@ -0,0 +1,7 @@
+(ns pre-commit-jira.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
