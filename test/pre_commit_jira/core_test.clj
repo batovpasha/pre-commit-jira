835bb2a8e8907d304969a41c0d6da7272006d008
Pavlo Batov <batovpasha@gmail.com>
2024-12-08 22:01:41 +0200

    chore: remove unused files from boilerplate

--- test/pre_commit_jira/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns pre-commit-jira.core-test
-  (:require [clojure.test :refer :all]
-            [pre-commit-jira.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
