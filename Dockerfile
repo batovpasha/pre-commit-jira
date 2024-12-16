148d454e56130d65447a3c43c3b8feda56640452
Pavlo Batov <batovpasha@gmail.com>
2024-12-16 10:16:07 +0200

    style: format Dockerfile and e2e-test.sh with shfmt

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,8 @@ FROM clojure:lein
 WORKDIR /usr/src/app
 
 # Target repo is mounted inside the container under /src directory. All files and folders
-# have the host user as user owner and "dialout" group as group owner, not the host one 
-# because it can't find the host group inside the container. 
+# have the host user as user owner and "dialout" group as group owner, not the host one
+# because it can't find the host group inside the container.
 # It causes the "fatal: detected dubious ownership in repository at '/src'" - user owner matches, but
 # group owner does not. The following command is a workaround to fix this issue.
 RUN git config --system --add safe.directory /src
--- e2e-test.sh
+++ e2e-test.sh
@@ -8,16 +8,16 @@ TMP_TEST_DIR="/tmp/pre-commit-jira-test"
 mkdir -p "${TMP_TEST_DIR}"
 cd "${TMP_TEST_DIR}"
 
-git init > /dev/null
+git init >/dev/null
 
-cat > .pre-commit-config.yaml <<EOF
+cat >.pre-commit-config.yaml <<EOF
 repos:
   - repo: https://github.com/batovpasha/pre-commit-jira
     rev: ${PRE_COMMIT_JIRA_VERSION}
     hooks:
       - id: add-jira-ticket
 EOF
-pre-commit install --hook-type commit-msg > /dev/null
+pre-commit install --hook-type commit-msg >/dev/null
 
 assert_equals() {
   local expected="$1"
@@ -34,8 +34,8 @@ assert_equals() {
 
 # Test cases
 TEST_CASE_1="should prepend JIRA ticket from branch to commit message"
-git checkout -b feature/TICKET-123-some-feature > /dev/null
-git commit --allow-empty -m "feat: add new feature" > /dev/null
+git checkout -b feature/TICKET-123-some-feature >/dev/null
+git commit --allow-empty -m "feat: add new feature" >/dev/null
 COMMIT_MSG=$(git log -1 --pretty=%B)
 assert_equals "feat: TICKET-123 add new feature" "${COMMIT_MSG}" "${TEST_CASE_1}"
 
