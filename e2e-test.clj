4edf534d934182814ce7a17c1a7c1b899a552577
Pavlo Batov <batovpasha@gmail.com>
2025-03-10 17:02:23 +0200

    test: add main test and setup/teardown hooks

--- e2e-test.clj
+++ e2e-test.clj
@@ -1,10 +1,14 @@
 #!/usr/bin/env bb
 
-(require '[babashka.process :refer [sh shell]]
+(require '[babashka.process :refer [sh shell *defaults*]]
          '[clojure.string :as str]
-         '[babashka.fs :as fs])
+         '[babashka.fs :as fs]
+         '[clojure.test :refer [use-fixtures deftest is run-tests]])
 
-(def is-pre-commit-installed (:exit (sh "command -v pre-commit")))
+(def is-pre-commit-installed
+  (-> (sh "command -v pre-commit")
+      :exit
+      zero?))
 (when-not is-pre-commit-installed
   (println (str "\033[0;31m"
                 "Please, install pre-commit to run e2e tests: "
@@ -14,39 +18,49 @@
 (def pre-commit-jira-version "v1.0.0")
 (def tmp-test-dir "/tmp/pre-commit-jira-test")
 
-(shell "mkdir" "-p" tmp-test-dir)
+(defn setup-git-repo
+  []
+  (shell "git init")
+  (shell "git config user.email" "batovpasha@gmail.com")
+  (shell "git config user.name" "Pavlo Batov"))
 
-(shell "git config --global user.email"
-       "batovpasha@gmail.com"
-       {:dir tmp-test-dir})
-(shell "git config --global user.name" "Pavlo Batov" {:dir tmp-test-dir})
-(shell "git init" {:dir tmp-test-dir})
-
-(def pre-commit-config-path
-  (str (fs/path tmp-test-dir ".pre-commit-config.yaml")))
 (def pre-commit-config-template
   "
 repos:
   - repo: https://github.com/batovpasha/pre-commit-jira
     rev: {{pre-commit-jira-version}}
     hooks:
-      -id: add-jira-ticket")
-(def pre-commit-config
-  (-> pre-commit-config-template
-      (str/replace "{{pre-commit-jira-version}}" pre-commit-jira-version)))
+      - id: add-jira-ticket
+")
+(defn setup-pre-commit
+  []
+  (let [pre-commit-config-path (str (fs/path tmp-test-dir
+                                             ".pre-commit-config.yaml"))
+        pre-commit-config      (-> pre-commit-config-template
+                                   (str/replace "{{pre-commit-jira-version}}"
+                                                pre-commit-jira-version))]
+    (spit pre-commit-config-path pre-commit-config)
+    (shell "pre-commit install --hook-type commit-msg")))
 
-(spit pre-commit-config-path pre-commit-config)
-(shell "pre-commit install --hook-type commit-msg" {:dir tmp-test-dir})
+(defn setup [] (setup-git-repo) (setup-pre-commit))
 
-;; TODO
-;; (deftest add-jira-ticket
-;;   ())
+(use-fixtures :once
+              (fn [f]
+                (shell "mkdir" "-p" tmp-test-dir)
+                (binding [*defaults* (merge *defaults* {:dir tmp-test-dir})]
+                  (setup)
+                  (f))
+                (shell "rm" "-rf" tmp-test-dir)))
 
-;; (t/deftest addition
-;;   (t/is (= 1 2)))
+(deftest add-jira-ticket
+  (shell "git checkout" "-b" "feature/TICKET-123-some-feature")
+  (shell "git commit --allow-empty" "-m" "feat: add new feature")
+  (let [expected (-> (shell {:out :string} "git --no-pager log -1 --pretty=%B")
+                     :out
+                     str/split-lines
+                     first)]
+    (is (= "feat: TICKET-123 add new feature" expected)
+        "should prepend JIRA ticket from branch to commit message")))
 
-;; (def test-results
-
-;; (let [{:keys [fail error]} test-results]
-;;   (when (pos? (+ fail error))
-;;     (System/exit 1)))
+(let [{:keys [fail error]} (run-tests)]
+  (when (pos? (+ fail error)) (System/exit 1)))
